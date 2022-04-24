25134b28d17523b43b2b195b6e0b5c1e3676e2bb
raaowx <raaowx@protonmail.com>
2022-04-24 09:44:42 +0200

    [ADD] Day 16

---
+++ Projects/wesplit/wesplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  wesplit
+//
+//  Created by Álvaro LDG on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
