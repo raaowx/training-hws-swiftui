f278ea005c07b68ab93d5976b180a736ef71dfd5
raaowx <raaowx@protonmail.com>
2023-02-27 17:08:42 +0100

    [ADD] Day 26

---
+++ Projects/betterrest/betterrest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  betterrest
+//
+//  Created by Álvaro LDG on 20/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projects/betterrest/betterrest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  betterrest
+//
+//  Created by Álvaro LDG on 20/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wakeUp = Date.now
+
+
+    var body: some View {
+        Text("CoreML")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
