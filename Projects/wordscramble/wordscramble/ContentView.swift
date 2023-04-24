363035507cb10c40e32fcdba0c0286e6ec947d6d
raaowx <raaowx@protonmail.com>
2023-04-24 15:18:58 +0200

    [ADD] Day 29

---
+++ Projects/wordscramble/wordscramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wordscramble
+//
+//  Created by Álvaro LDG on 28/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Color.black
+            .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projects/wordscramble/wordscramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  wordscramble
+//
+//  Created by Álvaro LDG on 28/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
